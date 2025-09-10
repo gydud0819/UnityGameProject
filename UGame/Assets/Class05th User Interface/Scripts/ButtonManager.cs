d0a9ac3a8a46c11267dc822b821d7e34e73edc19
gydud0819 <pstar0819@naver.com>
2025-09-10 15:45:21 +0900

    (2025-09-10) Event System

--- UGame/Assets/Class05th User Interface/Scripts/ButtonManager.cs
+++ UGame/Assets/Class05th User Interface/Scripts/ButtonManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,26 +6,53 @@ public class ButtonManager : MonoBehaviour
 {
     [SerializeField] Button[] buttonList;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    //[SerializeField] Text[] buttonName;
+    [SerializeField] string[] buttonName = new string[] {"Start", "Option", "Quit"};
+    Selectable selectable;
+    void Start()                                         
+    {                                                     
+        //buttonName = GetComponentInChildren<Text>();
+
+        Action[] actions = { Execute, Option, Quit };
+
+        for (int i = 0; i < actions.Length; i++)
+        {
+            int index = i;      // 변수 캡쳐,,, 메모,,, 
+
+            buttonList[i].GetComponentInChildren<Text>().text = buttonName[index];
+
+            buttonList[i].onClick.AddListener(() => actions[index]());     // 설마햇는데 진짜 람다식이엿네;;;;;;;;
+        }
+
+
+        //buttonName[0].text = "Start";
+        //buttonName[1].text = "Option";
+        //buttonName[2].text = "Quit";
+
+
+
     }
 
     public void Execute()
     {
-        
+       
+
         Debug.Log("Execute");
     }
-    
+
     public void Option()
     {
+
         Debug.Log("Option");
 
     }
 
     public void Quit()
     {
+
         Debug.Log("Quit");
-        
+
     }
+
+
 }
---
+++ UGame/Assets/Class05th User Interface/Scripts/Selectable.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.EventSystems;
+
+public class Selectable : MonoBehaviour
+{
+    // 이게 무슨말이야ㅏ;;;;;;;;;;
+    Button[] button;
+
+    public void Enter()
+    {
+        //button[0].GetComponentInChildren<Text>().text = 
+    }
+    public void Exit()
+    {
+
+    }
+
+    public void Down()
+    {
+
+    }
+}
