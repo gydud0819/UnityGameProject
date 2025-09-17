155001e09579a62fe98134e18d2de3b6fa9129df
gydud0819 <pstar0819@naver.com>
2025-09-17 15:46:08 +0900

    (2025-09-17) 코루틴

--- UGame/Assets/Class09th Instantiate/Scripts/CreateManager.cs
+++ UGame/Assets/Class09th Instantiate/Scripts/CreateManager.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,32 +10,25 @@ public class CreateManager : MonoBehaviour
     GameObject game;
     [SerializeField] List<GameObject> prefabs;
     [SerializeField] float time;     // 5.0 붙여야하나 
+
+
+
     private void Start()
     {
         //game.transform.position = new Vector3(1, 0, 0);
 
         //for (int i = 0; i < 5; i++)
         //{
-
         //    prefabs[i].SetActive(false);
         //    prefabs.Add(Instantiate(prefab));
         //}
 
         Initialized();
+        StartCoroutine(Coroutine());
 
     }
 
-    private void Update()
-    {
-        time += Time.deltaTime;
-
-        if(time >= 5.0)
-        {
-            Debug.Log("호출");
-        }
-    }
-
-     void Initialized()
+    void Initialized()
     {
         prefabs.Capacity = 10;
         for (int i = 0; i < count; i++)
@@ -43,6 +37,68 @@ public class CreateManager : MonoBehaviour
             game.SetActive(false);
             prefabs.Add(game);
         }
+
+    }
+
+    bool ExmineActive()
+    {
+        for (int i = 0; i < prefabs.Count; i++)
+        {
+            if (prefabs[i].activeSelf == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    IEnumerator Coroutine()
+    {
+        int idx = 0;
+
+        while (true)
+        {
+            // 5초마다 하나씩 생성시키되 같은 수가 나오면 그 다음걸 실행시키기..?
+            yield return new WaitForSeconds(5.0f);      // yield : 양보하다(?)
+
+            idx = Random.Range(0, count);
+
+            if (prefabs[idx].activeSelf)
+            {
+                //idx = (idx + 1) % prefabs.Count;
+                continue;   // 얘도 될거같은디 
+            }
+
+            prefabs[idx].SetActive(true);
+
+            if(ExmineActive())
+            {
+                yield break;
+            }
+
+            //while (prefabs[idx].activeSelf)
+            //{
+            //    idx = Random.Range(0, count);
+            //}
+
+            //for (int i = 0; i < count; i++)
+            //{
+            //    if (prefabs[i].activeSelf)
+            //    {
+            //        index++;
+            //    }
+
+            //}
+            //if (index == count)
+            //{
+            //    yield break;
+
+            //}
+
+            //이게 탈출 조건 ?
+        }
+
     }
 
 }
